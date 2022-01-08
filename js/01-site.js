9bd6779e430290c0a1d99013eba76d6baf4da249
Ben Hall <benhall.ninetysix@gmail.com>
2022-01-08 17:44:25 +0000

    Circular progress bar animated

--- js/01-site.js
+++ js/01-site.js
@@ -1,1 +1,27 @@
 "use strict";
+
+// Course Card Progress
+const progressPath = document.querySelector('#icon--progress #progress-bar');
+
+let pathLength = progressPath.getTotalLength();
+let percent = document.querySelector('.course-card__progress').dataset.percentage;
+
+if(progressPath){
+    window.onload = () => {
+        progressPath.animate([
+            // keyframes
+            {
+                strokeDasharray: pathLength + ' ' + pathLength,
+                strokeDashoffset: pathLength
+            },
+            { strokeDashoffset: pathLength * ((100 - percent) / 100) }
+        ], {
+            // timing options
+            duration: 1000,
+            easing: 'ease-in-out',
+            iterations: 1,
+            direction: 'alternate',
+            fill: 'forwards'
+        });
+    };
+}
